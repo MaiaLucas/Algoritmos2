1d81545464e1ab08332888372f39933ca4cbc774
Lucas Eduardo <33419659+MaiaLucas@users.noreply.github.com>
2019-02-26 09:16:38 -0300

    Create app.4.js

---
+++ src/app.4.js
@@ -0,0 +1,16 @@
+import Tree from './components/tree/binarySearchTree';
+
+
+let tree = new Tree();
+//tree.add(13);
+let keys = [ 13, 10, 40, 34, 20, 8, 11, 7 ];
+
+keys.forEach(key => tree.add(key) );
+
+let node = tree.search(7);
+
+
+/**/
+console.log(node)
+
+
