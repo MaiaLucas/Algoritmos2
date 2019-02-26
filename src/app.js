5d2869d828e947d2c17058554bbeb43de66e3af3
Lucas Eduardo <33419659+MaiaLucas@users.noreply.github.com>
2019-02-26 09:16:42 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,14 +1,20 @@
 import Tree from './components/tree/binarySearchTree';
+import { printKey, printMenu } from './util/transversalOperations';
 
 
 let tree = new Tree();
 //tree.add(13);
 let keys = [ 13, 10, 40, 34, 20, 8, 11, 7 ];
 
-keys.forEach(key => tree.add(key) );
+keys.forEach(key => tree.otherAdd(key) );
 
-let node = tree.search(7);
+/**/
+//console.log(tree);
 
-console.log(node)
+//-- Percusos
+//tree.preOrder(printMenu);
+//tree.inOrder();
+//tree.posOrder(printMenu);
 
-
+//-- Largura
+tree.ifLeaf();
