772c33e842f543bf07c68af64f13340d56e4bfc4
Lucas Eduardo <33419659+MaiaLucas@users.noreply.github.com>
2019-03-14 08:43:00 -0300

    Update transversalOperations.js

--- src/util/transversalOperations.js
+++ src/util/transversalOperations.js
@@ -1,15 +1,11 @@
-import Node from '../components/tree/node';
-
 export const printKey = (node) => {
-    console.log(node.key);
+    console.log(node.key)
 }
 
-export const printMenu = (node) => {
-
-    let pattern = '';
-    for( let i = 0; i < node.depth(); i++ ){
-        pattern += "| ";
+export const printMenuKey = (node) => {
+    let pattern = ""
+    for (let i = 0; i < node.depth(); i++) {
+        pattern += "|  "
     }
-     
     console.log(`${pattern}-> ${node.key}`)
 }
