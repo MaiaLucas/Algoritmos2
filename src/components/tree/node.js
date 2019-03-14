bfff9ef4d99e95c66a2146d103e7d4e05ceb3ec6
Lucas Eduardo <33419659+MaiaLucas@users.noreply.github.com>
2019-03-14 08:42:49 -0300

    Update node.js

--- src/components/tree/node.js
+++ src/components/tree/node.js
@@ -1,9 +1,33 @@
 export default class Node {
-    constructor(key, parent = null) {
-        this.key        = key;
-        this.parent     = parent;
-        this.leftChild  = null;
+    constructor(key, parent=null) {
+        this.key = key;
+        this.parent = parent;
+        this.leftChild = null;
         this.rightChild = null;
+    }
+
+    isLeaf() {
+        return (this.leftChild == null && this.rightChild == null)
+    }
+
+    hasBothChildren() {
+        return (this.leftChild !== null && this.rightChild !== null)
+    }
+
+    isLeftChild() {
+        return this.parent.leftChild === this
+    }
+
+    isRightChild() {
+        return this.parent.rightChild === this
+    }
+    
+    hasLeftChild() {
+        return this.leftChild !== null
+    }
+
+    hasRightChild() {
+        return this.rightChild !== null
     }
 
     isRoot() {
@@ -11,51 +35,7 @@ export default class Node {
     }
 
     depth() {
-        if( this.isRoot() ) return 0;
-
+        if(this.isRoot()) return 0
         return 1 + this.parent.depth()
     }
-
-    /**
-     * It returns true if the node is a leaf
-     */
-    isLeaf() {
-        return this.leftChild == null && this.rightChild == null 
-    }
-
-    /**
-     * It returns true if the node has both children 
-     */
-    hasBothChildren() {
-        return this.leftChild != null && this.rightChild != null
-    }
-
-    /**
-     * It returns true if this node is a left child
-     */
-    ifLeftChild() {
-         
-    }
-
-    /**
-     * It returns true if this node has a left child
-     */
-    hasLeftChild() {
-        return this.leftChild !== null;
-    }
-
-    /**
-     * it returns true if this node is a left child
-     */
-    ifRightChild() {
-        
-    }
-
-    /**
-     * It returns true if this node has a left child
-     */
-    hasRightChild() {
-        return this.rightChild !== null;
-    }
-
 }
