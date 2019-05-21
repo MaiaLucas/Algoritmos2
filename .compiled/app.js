a6991ecf87161afeef2c2c1cd4db119355798f80
Lucas Eduardo <33419659+MaiaLucas@users.noreply.github.com>
2019-05-21 08:31:20 -0300

    .compiled

--- .compiled/app.js
+++ .compiled/app.js
@@ -8,6 +8,10 @@ var _VertexList = require('./components/graph.list/VertexList');
 
 var _VertexList2 = _interopRequireDefault(_VertexList);
 
+var _EdgeList = require('./components/graph.list/EdgeList');
+
+var _EdgeList2 = _interopRequireDefault(_EdgeList);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 // Vextexes
@@ -17,10 +21,21 @@ var w = new _VertexList2.default('w');
 var z = new _VertexList2.default('z');
 
 //Edges
-var e = new Edge(v, u);
-var g = new Edge(u, w);
-var f = new Edge(v, w);
-var i = new Edge(w, z);
+var e = new _EdgeList2.default(v, u);
+var g = new _EdgeList2.default(u, w);
+var f = new _EdgeList2.default(v, w);
+var i = new _EdgeList2.default(w, z);
 
 var outcoming = new Map();
+outcoming.set(u, []);
+outcoming.set(v, []);
+outcoming.set(w, []);
+outcoming.set(z, []);
+
+outcoming.get(u).push(e, g);
+outcoming.get(v).push(e, f);
+outcoming.get(w).push(f, g, i);
+outcoming.get(z).push(i);
+
+console.log(outcoming.get(u));
 //# sourceMappingURL=app.js.map
