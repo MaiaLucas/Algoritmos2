87141da191429d353dd2c0ba6020b418cb53f90f
Lucas Eduardo <33419659+MaiaLucas@users.noreply.github.com>
2019-05-23 08:48:32 -0300

    Operações no Grafo

--- src/util/graphOpration.js
+++ src/util/graphOpration.js
@@ -1,3 +1,10 @@
 export const DFS = (graph, vertex, discovered) => { //Busca em profundidade 
+    graph.incidentEdge(vertex).forEach( edge => {
+        let opposite = edge.opposite(vertex)
 
+        if( !discovered.has(opposite) ) {
+            discovered.set(opposite, edge)
+            DFS(graph, opposite, discovered)
+        }
+    } );
 }
