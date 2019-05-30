608d38da541a7082d003d29d565b69bb5ca5add4
Lucas Eduardo <33419659+MaiaLucas@users.noreply.github.com>
2019-05-30 08:36:27 -0300

    Implementação do constructPath

--- src/util/graphOpration.js
+++ src/util/graphOpration.js
@@ -8,3 +8,24 @@ export const DFS = (graph, vertex, discovered) => { //Busca em profundidade
         }
     } );
 }
+
+export const constructPath = (origin, destination, discovered) => {
+    let path = []
+
+    if( discovered.has(destination) ) {
+        let step = destination
+        path.push(step)
+
+        while( step !== origin ) {
+            let edge = discovered.get(step)
+            let opposite = edge.opposite(step)
+            path.push(opposite)
+
+            step = opposite
+        }
+
+        path.reverse()
+    }
+
+    return path
+}
